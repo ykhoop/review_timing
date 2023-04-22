a6b214c869e07cb1432b32ecdfc44988ae953dbb
ykhoop <yk-hoooooop@live.jp>
2023-04-22 13:45:46 +0000

    [Add] 科目の一覧、編集、削除機能を追加

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,6 @@
 class SubjectsController < ApplicationController
+  before_action :set_subcject, only: %i[edit update destroy]
+
   def new
     @subject = Subject.new
   end
@@ -6,14 +8,38 @@ class SubjectsController < ApplicationController
   def create
     @subject = current_user.subjects.build(subject_params)
     if @subject.save
-      redirect_to top_path, success: t('.success')
+      redirect_to subjects_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
       render :new, status: :unprocessable_entity
     end
   end
 
+  def index
+    @subjects = current_user.subjects
+  end
+
+  def edit; end
+
+  def update
+    if @subject.update(subject_params)
+      redirect_to subjects_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @subject.destroy!
+    redirect_to subjects_path, success: t('.success', item: @subject.title)
+  end
+
   private
+
+  def set_subcject
+    @subject = current_user.subjects.find(params[:id])
+  end
 
   def subject_params
     params.require(:subject).permit(:title, :start_at, :limit_at, :memo)
