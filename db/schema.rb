257bfe19dd9909bf6651da4e528a99c27bc38dc5
ykhoop <yk-hoooooop@live.jp>
2023-05-01 15:49:30 +0000

    [Fix] Rubyバージョンアップ、テンプレート適用

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_135320) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_23_022723) do
+  create_table "subject_details", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "subject_id", null: false
+    t.string "chapter"
+    t.integer "start_page"
+    t.integer "end_page"
+    t.datetime "start_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_subject_details_on_subject_id"
+  end
+
+  create_table "subject_reviews", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "subject_detail_id", null: false
+    t.integer "review_type", null: false
+    t.integer "review_number", null: false
+    t.datetime "review_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_detail_id"], name: "index_subject_reviews_on_subject_detail_id"
+  end
+
   create_table "subjects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "start_at"
@@ -34,5 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_135320) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "subject_details", "subjects"
+  add_foreign_key "subject_reviews", "subject_details"
   add_foreign_key "subjects", "users"
 end
