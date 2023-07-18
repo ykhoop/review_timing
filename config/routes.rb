da595605c65b72a0d588447d67f47468dbb87245
ykhoop <yk-hoooooop@live.jp>
2023-07-18 13:17:23 +0000

    [Add] 管理者用 登録件数一覧を追加

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+    resources :dashboards, only: %i[index]
     resources :users, only: %i[index show edit update destroy]
   end
 
