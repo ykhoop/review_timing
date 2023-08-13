a28f8b979e4bad065eac4cb1987cfb64942bf4af
ykhoop <yk-hoooooop@live.jp>
2023-08-13 05:45:40 +0000

    [Add] ログイン後のパスワード変更機能の追加

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,42 @@
+class PasswordsController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
+  def edit; end
+
+  def update
+    current_password = params[:user][:current_password]
+    password = params[:user][:password]
+
+    if User.authenticate(@user.email, current_password).nil?
+      render_when_update_fail(t('.err_msg_current_password_not_match'))
+      return
+    end
+
+    if password.blank?
+      render_when_update_fail(t('.err_msg_password_blank'))
+      return
+    end
+
+    if @user.update(user_params)
+      redirect_to password_users_path, success: t('.success')
+    else
+      render_when_update_fail
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:current_passowrd, :password, :password_confirmation)
+  end
+
+  def render_when_update_fail(err_msg = nil)
+    @user.errors.add(:base, err_msg) if err_msg.present?
+    flash.now[:danger] = t('.fail')
+    render :edit, status: :unprocessable_entity
+  end
+end
