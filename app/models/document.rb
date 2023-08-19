1e10dc32cf0ecd7e317687c8d0bb4117ed79c085
ykhoop <yk-hoooooop@live.jp>
2023-08-19 06:12:19 +0000

    [Add] お問い合わせ取得メソッドを追加

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,8 @@ class Document < ApplicationRecord
   validates :code, presence: true
   validates :description, presence: true
   validates :content, presence: true
+
+  def self.contact_url
+    Document.find_by(code: 'contact').content
+  end
 end
