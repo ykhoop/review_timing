f825fbc3b6c5fde016abd98f87fe446c23c4de51
ykhoop <yk-hoooooop@live.jp>
2023-11-19 06:40:36 +0000

    [Fix] ログイン後のメニューの位置修正等

--- app/models/subject_review.rb
+++ app/models/subject_review.rb
@@ -7,13 +7,17 @@ class SubjectReview < ApplicationRecord
   enum review_type: { plan: 0, actual: 1 }
 
   def self.create_reviews!(subject_detail, user)
+    counts_of_review_type = 2
+    counts_of_review_days = 4
+    val_not_set_review_at = 0
+
     review_days = user.user_review_settings.order(:review_number).pluck(:review_days)
-    2.times do |i|
-      4.times do |j|
+    counts_of_review_type.times do |i|
+      counts_of_review_days.times do |j|
         subject_review = subject_detail.subject_reviews.build
         subject_review.review_type = i
         subject_review.review_number = j + 1
-        i == 0 && review_days[j].days != 0 ? subject_review.review_at = subject_detail.start_at + review_days[j].days : nil
+        i == SubjectReview.review_types[:plan] && review_days[j].days != val_not_set_review_at ? subject_review.review_at = subject_detail.start_at + review_days[j].days : nil
         subject_review.save!
       end
     end
